a4b818ddbbc117f87f4aacd1109e43bdce700297
avi <hi@avi.im>
2020-11-24 18:59:14 +0530

    complete log to query with tests

--- mltq.go
+++ mltq.go
@@ -2,17 +2,47 @@ package mltq
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
+type LogLine struct {
+	C    string `json:"c"`
+	Attr struct {
+		Type    string `json:"type"`
+		Command struct {
+			Find      string          `json:"find"`
+			Filter    json.RawMessage `json:"filter"`
+			Sort      json.RawMessage `json:"sort,omitempty"`
+			Limit     int             `json:"limit"`
+			MaxTimeMS int             `json:"maxTimeMS"`
+			Db        string          `json:"$db"`
+		} `json:"command"`
+	} `json:"attr"`
+}
+
 func LogToQuery(log string) (string, error) {
-	line := make(map[string]string)
+	var line LogLine
 	err := json.Unmarshal([]byte(log), &line)
 	if err != nil {
 		return "", err
 	}
-	cmd, ok := line["c"]
-	if !ok || cmd != "COMMAND" {
+	if !(line.C == "COMMAND" && line.Attr.Type == "command") {
+		fmt.Println(line.C, line.Attr.Type)
 		return "", nil
 	}
-	return line["attr"], nil
+	command := line.Attr.Command
+	if command.Find == "" {
+		return "", nil
+	}
+	result := fmt.Sprintf(`db.%s.find(%s)`, command.Find, command.Filter)
+	if command.Sort != nil {
+		result = fmt.Sprintf(`%s.sort(%s)`, result, command.Sort)
+	}
+	if command.Limit != 0 {
+		result = fmt.Sprintf(`%s.limit(%d)`, result, command.Limit)
+	}
+	if command.MaxTimeMS != 0 {
+		result = fmt.Sprintf(`%s.maxTimeMS(%d)`, result, command.MaxTimeMS)
+	}
+	return result, nil
 }
